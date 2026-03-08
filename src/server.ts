b1b6b1541869013850e6196e17df24e2e73335db
wendel kaua <wendellkauan870@gmail.com>
2026-03-07 21:36:36 -0300

    feat: add rooms listing

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import fastifyCors from "@fastify/cors";
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
+import { getRoomsRoutes } from "./http/routes/get-rooms.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5173",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(getRoomsRoutes);
+
+app.listen({ host: "0.0.0.0", port: env.PORT }).then(() => {});
